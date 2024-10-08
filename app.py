c9ef99df5e9a229fe82ad6bee467874539edf099
Artem Smirnov <62436241+Smirnov-Artem@users.noreply.github.com>
2024-10-08 19:14:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def index():
         return render_template('index.html', title=title, intro=intro_text)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
