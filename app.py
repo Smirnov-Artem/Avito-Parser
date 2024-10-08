dc3522618a3d43a49aa8f1841393c4afc92cbb79
Artem Smirnov <62436241+Smirnov-Artem@users.noreply.github.com>
2024-10-08 19:27:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -14,19 +14,36 @@ chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Путь к chromedriver
-    chrome_service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH", "/usr/bin/chromedriver"))
+    result = None
 
-    # Запускаем chromedriver
-    with webdriver.Chrome(service=chrome_service, options=chrome_options) as driver:
-        driver.get('https://www.python.org/')
-        title = driver.title
-        element = driver.find_element(By.CLASS_NAME, 'introduction')
-        intro_text = element.text
-        # Возвращаем результат работы браузера
-        return render_template('index.html', title=title, intro=intro_text)
+    if request.method == 'POST':
+        # Получаем URL от пользователя
+        url = request.form.get('url')
+
+        if url:
+            # Путь к chromedriver
+            chrome_service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH", "/usr/bin/chromedriver"))
+
+            # Запускаем chromedriver и парсим контент страницы
+            with webdriver.Chrome(service=chrome_service, options=chrome_options) as driver:
+                driver.get(url)
+                title = driver.title
+                try:
+                    # Находим первый параграф на странице
+                    element = driver.find_element(By.TAG_NAME, 'p')
+                    paragraph = element.text
+                except:
+                    paragraph = "No paragraph found on this page."
+
+                result = {
+                    'url': url,
+                    'title': title,
+                    'paragraph': paragraph
+                }
+
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
